b7831002b515aac0a05112ef601628ef5ca56dbd
Nic McPhee <mcphee@morris.umn.edu>
2020-06-03 17:45:20 -0500

    Implement basic EDN->Forest translation
    This allows us to build forests. It takes quite a while (over half an hour) to load 116 generations (116,000 individuals) into a forest on my iMac.

--- src/pushgp_similarity/core.clj
+++ src/pushgp_similarity/core.clj
@@ -1,8 +1,31 @@
 (ns pushgp-similarity.core
-  (:require [consimilo.core :as consimilo])
+  (:require [consimilo.core :as consimilo]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn individual->feature
+  [individual]
+  {:id (:uuid individual)
+   :generation (:generation individual)
+   :features (:errors individual)
+   :total-error (:total-error individual)})
+
+(defn edn-file->features
+  [filename]
+  (with-open [reader (io/reader filename)]
+    (doall
+     (map
+      #(->> %
+            (edn/read-string {:default (fn [t v] v)})
+            (individual->feature))
+      (rest (line-seq reader))))))
+
+(defn edn-file->forest
+  [filename]
+  (consimilo/add-all-to-forest (edn-file->features filename)))
