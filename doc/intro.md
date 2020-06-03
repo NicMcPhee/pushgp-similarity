3fca295ee62d39f9faca5ab988256ae9e02a1f9f
Nic McPhee <mcphee@morris.umn.edu>
2020-06-03 13:27:09 -0500

    Initial lein app project
    I added `data` to .gitignore` so I can put big EDN files in there and no worry about them accidentally being added to the repository.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pushgp-similarity
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/pushgp_similarity/core.clj
@@ -0,0 +1,8 @@
+(ns pushgp-similarity.core
+  (:require [consimilo.core :as consimilo])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
